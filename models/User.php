b3ce3a3cba9140782043e7e898e4cff14a68e017
Tomoki Morita <tmsongbooks215@gmail.com>
2015-01-02 23:52:33 +0900

    Fix import class

--- models/User.php
+++ models/User.php
@@ -3,9 +3,10 @@
 namespace app\models;
 
 use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 use yii\web\IdentityInterface;
-use yii\behaviors\TimestampBehavior;
 
 /**
  * User class file.
