f537182f55e20314ec6d5e03c82c18e99282a85f
Tomoki Morita <tmsongbooks215@gmail.com>
2015-04-23 07:09:55 +0900

    Move the position of font-awasome.css

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -21,13 +21,12 @@ CommonAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title . ' - ' . Yii::$app->name) ?></title>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <?php $this->head() ?>
-
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
 <?php $this->beginBody() ?>
