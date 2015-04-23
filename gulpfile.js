fc67709e17a83e92ecc462df33c276ed2c7d4e50
Tomoki Morita <tmsongbooks215@gmail.com>
2015-04-24 08:58:26 +0900

    Remove unnecessary string

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,38 @@ var reload = browserSync.reload;
 // font
 gulp.task('font', function() {
   gulp.src([
-      './vendor/bower/bootstrap/dist/fonts/*'
+      'vendor/bower/bootstrap/dist/fonts/*'
   ])
-    .pipe(gulp.dest('./assets/public/fonts'));
+    .pipe(gulp.dest('assets/public/fonts'));
 });
 
 // js
 gulp.task('js', function() {
   gulp.src([
-    './vendor/bower/jquery/dist/jquery.min.js',
-    './vendor/bower/bootstrap/dist/js/bootstrap.min.js',
-    './vendor/yiisoft/yii2/assets/**/*.js',
-    './vendor/bower/yii2-pjax/jquery.pjax.js',
-    './vendor/bower/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js',
-    './vendor/bower/toastr/toastr.min.js',
+    'vendor/bower/jquery/dist/jquery.min.js',
+    'vendor/bower/bootstrap/dist/js/bootstrap.min.js',
+    'vendor/yiisoft/yii2/assets/**/*.js',
+    'vendor/bower/yii2-pjax/jquery.pjax.js',
+    'vendor/bower/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js',
+    'vendor/bower/toastr/toastr.min.js',
   ])
     .pipe($.concat('common.js'))
     .pipe($.uglify({preserveComments: 'some'}))
-    .pipe(gulp.dest('./assets/public/js'));
+    .pipe(gulp.dest('assets/public/js'));
 });
 
 // style
 gulp.task('style', function() {
   gulp.src([
-    './assets/less/_.less',
-    './vendor/bower/toastr/toastr.min.css',
+    'assets/less/_.less',
+    'vendor/bower/toastr/toastr.min.css',
   ])
     .pipe($.plumber())
     .pipe($.less())
     .pipe($.concat('common.css'))
     .pipe($.autoprefixer('last 2 version', 'ie 8'))
     .pipe($.minifyCss())
-    .pipe(gulp.dest('./assets/public/css'))
+    .pipe(gulp.dest('assets/public/css'))
     .pipe(reload({stream: true, once: true}));
 });
 
@@ -47,20 +47,20 @@ gulp.task('style', function() {
 gulp.task('serve', function() {
   browserSync({
     notify: false,
-    sever: {baseDir: './web'}
+    sever: {baseDir: 'web'}
   });
 
-  gulp.watch('./assets/less/*.less', ['style']);
-  gulp.watch('./{controllers,views}/**/*.php', reload);
+  gulp.watch('assets/less/*.less', ['style']);
+  gulp.watch('{controllers,views}/**/*.php', reload);
 });
 
 // clean
 gulp.task('clean', del.bind(null, [
-  './node_modules',
-  './runtime/*',
-  './tests/codeception/_output/*',
-  './vendor',
-  './web/assets/*'
+  'node_modules',
+  'runtime/*',
+  'tests/codeception/_output/*',
+  'vendor',
+  'web/assets/*'
 ]));
 
 // build
