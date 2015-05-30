d412f63304243c81195e7d946fec09686744e57c
Tomoki Morita <tmsongbooks215@gmail.com>
2015-05-30 12:27:52 +0900

    Update browser-sync

--- views/common/browser-sync.php
+++ views/common/browser-sync.php
@@ -1,3 +1,3 @@
 <script type='text/javascript' id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.6.4.js'><\/script>".replace("HOST", location.hostname));
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.7.6.js'><\/script>".replace("HOST", location.hostname));
 //]]></script>
